48bb1096982d8d4fb14bc75456416f107c076728
Allison Dungo <adung002@odu.edu>
2021-10-07 00:19:33 -0400

    Added # of expected storms vs # of actual storms

--- main.py
+++ main.py
@@ -9,4 +9,12 @@ import numpy as np
 if __name__ == '__main__':
     may_outlook = pd.read_csv("may_hurricane_outlook.csv")
     hurricanes = pd.read_csv("hurricane_data_after.csv")
-    print(hurricanes.date)
+    summaries = pd.read_csv("hurricane_summaries.csv")
+
+    years = may_outlook.year.unique() # put individual years into an array
+
+    for x in years:
+        expected_storms = may_outlook['# of expected named storms'][may_outlook['year'] == x]
+        print("Year: ", x)
+        print("# of Expected Named Storms: ", expected_storms.values)
+        print("# of Actual Storms: ", len(summaries[summaries['year'] == x]), "\n")
